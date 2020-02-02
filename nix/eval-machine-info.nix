e961ac00f8cbaf066facab09ca3adff6ab319232
Shay Bergmann <shaybergmann@gmail.com>
2020-02-02 17:04:18 -0500

    Fix <nixpkgs> leak

--- nix/eval-machine-info.nix
+++ nix/eval-machine-info.nix
@@ -29,7 +29,7 @@ rec {
   pluginDeploymentConfigExporters = (foldl (a: e: a ++ (e.config_exporters { inherit optionalAttrs pkgs; })) [] importedPluginNixExprs);
 
   network = let
-    baseModules = import (nixpkgs + "/nixos/modules/module-list.nix");
+    baseModules = import (pkgs.path + "/nixos/modules/module-list.nix");
 
   in (evalModules {
     modules = [
